e730fd109041dc7187dde346b94d436c269c81c0
jusan00b <babusravansiddartha@lpu.in>
2024-01-04 20:30:51 +0530

    changed theme

--- config.h
+++ config.h
@@ -75,7 +75,7 @@ static char dmenumon[2] =
     "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = {
     "dmenu_run", "-m",  dmenumon,  "-fn", dmenufont, "-nb", col_gray2, "-nf",
-    col_gray4,   "-sb", col_gray5, "-sf", col_gray5, NULL};
+    col_gray4,   "-sb", col_gray2, "-sf", col_gray5, NULL};
 static const char *termcmd[] = {"st", NULL};
 static const char *web[] = {"brave", NULL};
 static const char *poweroff[] = {"poweroff", NULL};
